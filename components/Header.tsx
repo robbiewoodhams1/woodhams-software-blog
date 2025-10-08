4da21b2e55d5b42be4bf54f7374a30af3dcbb704
Robbie Woodhams <robbiewoodhams643@gmail.com>
2025-10-08 16:59:38 +0100

    copied blog template

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
+import Image from 'next/image'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -17,7 +17,7 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Logo />
+            <Image src="/static/images/Logo.png" alt="Logo" width={60} height={60} priority />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
